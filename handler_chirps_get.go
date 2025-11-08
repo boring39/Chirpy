da2df3d64a9c10bb1ab0b2aff66378b9c2881bd4
boring39 <boring.mike@hotmail.com>
2025-11-08 21:15:26 +0900

    CH5 L10

--- handler_chirps_get.go
+++ handler_chirps_get.go
@@ -3,7 +3,39 @@ package main
 import (
 	"log"
 	"net/http"
+
+	"github.com/google/uuid"
 )
+
+func (cfg *apiConfig) handlerChirpsGet(w http.ResponseWriter, r *http.Request) {
+	type errorResponse struct {
+		Error string `json:"error"`
+	}
+
+	pathValue := r.PathValue("chirpId")
+	id, err := uuid.Parse(pathValue)
+	if err != nil {
+		log.Printf("Failed to parse UUID (%s): %s\n", pathValue, err.Error())
+		respondWithJSON(errorResponse{Error: "Something went wrong"}, http.StatusBadRequest, w)
+		return
+	}
+
+	chirp, err := cfg.db.GetChirp(r.Context(), id)
+	if err != nil {
+		log.Printf("Chirp UUID (%s) not found: %s", id, err.Error())
+		respondWithJSON(errorResponse{Error: "Chirp Id not found"}, http.StatusNotFound, w)
+		return
+	}
+
+	response := Chirp{
+		Id:        chirp.ID,
+		CreatedAt: chirp.CreatedAt,
+		UpdatedAt: chirp.UpdatedAt,
+		Body:      chirp.Body,
+		UserId:    chirp.Userid,
+	}
+	respondWithJSON(response, http.StatusOK, w)
+}
 
 func (cfg *apiConfig) handlerChirpsRetrieve(w http.ResponseWriter, r *http.Request) {
 	type errorResponse struct {
--- internal/database/chirps.sql.go
+++ internal/database/chirps.sql.go
@@ -42,6 +42,24 @@ func (q *Queries) CreateChirp(ctx context.Context, arg CreateChirpParams) (Chirp
 	return i, err
 }
 
+const getChirp = `-- name: GetChirp :one
+SELECT id, created_at, updated_at, body, userid FROM chirps
+WHERE id = $1
+`
+
+func (q *Queries) GetChirp(ctx context.Context, id uuid.UUID) (Chirp, error) {
+	row := q.db.QueryRowContext(ctx, getChirp, id)
+	var i Chirp
+	err := row.Scan(
+		&i.ID,
+		&i.CreatedAt,
+		&i.UpdatedAt,
+		&i.Body,
+		&i.Userid,
+	)
+	return i, err
+}
+
 const getChirps = `-- name: GetChirps :many
 SELECT id, created_at, updated_at, body, userid FROM chirps
 ORDER BY created_at ASC
